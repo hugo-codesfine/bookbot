69415d7fd0137efceacc00d442ee61cbe440f137
hugo-codesfine <146536465+hugo-codesfine@users.noreply.github.com>
2025-12-22 14:05:14 +0100

    count words function in main

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from stats import count_words
+
 
 def get_book_text(path_to_file):
     with open(path_to_file, encoding="utf-8") as f:
         book_contents = f.read()
     return book_contents
 
+        
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
+    #print(get_book_text("books/frankenstein.txt"))
 
 main()
