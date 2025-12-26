b8585f2e971a5a1864bc05a960fc4ea8dc36e782
hugo-codesfine <146536465+hugo-codesfine@users.noreply.github.com>
2025-12-26 13:19:26 +0000

    add sorted char function & return report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sorted_characters
 
 
 def get_book_text(path_to_file):
@@ -12,8 +12,14 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
     num_characters = count_characters(book_text)
+    sorted_list = sorted_characters(num_characters)
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_characters)
+    print("--------- Character Count -------")
+    for i in sorted_list:
+        print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
+    #print(sorted_list['char'])
     #print(get_book_text("books/frankenstein.txt"))
 
 main()
--- stats.py
+++ stats.py
@@ -16,6 +16,28 @@ def count_characters(book_as_string):
             character_dict[ch] += 1
     return character_dict
 
+def sort_on(list_of_dictionaries):
+    return list_of_dictionaries["num"]
+
+def sorted_characters(dictionary_of_characters): 
+    characters_list = []
+    for i in dictionary_of_characters:
+        if i.isalpha() != True:
+            continue
+        small_dict = {}
+        small_dict["char"] = i
+        small_dict["num"] = dictionary_of_characters[i]
+        characters_list.append(small_dict)
+    characters_list.sort(reverse=True, key=sort_on)
+    return characters_list
+
+
+    
+
+"""sample_chars = {"a": 10, "b": 5, "c": 15}
+result = sorted_characters(sample_chars)
+print(result)"""
+
 
 """if __name__ == "__main__":
     text = "AAa aBb!!!"
